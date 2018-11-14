4388ba7f68ddace731fab6d53671a2ceac60178b
Sapir <sapirbusybee@gmail.com>
2018-11-14 13:05:20 +0200

    first commit

---
+++ src/main/java/QueryPrivilegedEntities.java
@@ -0,0 +1,9 @@
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClientBuilder;
+import com.amazonaws.services.identitymanagement.model.GetPolicyRequest;
+import com.amazonaws.services.identitymanagement.model.GetPolicyResult;
+
+public class QueryPrivilegedEntities {
+
+
+}
